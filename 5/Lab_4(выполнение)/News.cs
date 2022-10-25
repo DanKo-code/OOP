cf6bf12889953b17db40fa2b20c195449820e9a2
Danila <danik2003globin@gmail.com>
2022-10-25 11:43:15 +0300

    сделал класс News partial

--- 5/Lab_4(выполнение)/News.cs
+++ 5/Lab_4(выполнение)/News.cs
@@ -8,7 +8,7 @@ namespace Lab_4performance
 {
     public partial class Program
     {
-        sealed public class News : TvProgram
+        sealed public partial class News : TvProgram
         {
             public News(int duration)
             {
@@ -27,28 +27,7 @@ namespace Lab_4performance
 
             public override string Define => "Начинаются новости!!!";
 
-            public override string ToString()
-            {
-                return $"Type: {this.GetType()}, Duration: {Duration}, Name: {Name}, Define: {Define}";
-            }
-
-            public override int GetHashCode()
-            {
-                return (int)(this.Duration - 456 * 10 / 20 + 9786 - 3);
-            }
-
-            private int id;
-
-            public override bool Equals(object obj)
-            {
-                if (obj == null) return false;
-                if (obj.GetType() != this.GetType()) return false;
-                News test_equals = (News)obj;
-                return ((this.Duration == test_equals.Duration) &&
-                        (this.Define == test_equals.Define) &&
-                        (this.Name == test_equals.Name) &&
-                        (this.id == test_equals.id));
-            }
+            
         }
     }
 }
---
+++ 5/Lab_4(выполнение)/News2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4performance
+{
+    public partial class Program
+    {
+        sealed public partial class News : TvProgram
+        {
+            public override string ToString()
+            {
+                return $"Type: {this.GetType()}, Duration: {Duration}, Name: {Name}, Define: {Define}";
+            }
+
+            public override int GetHashCode()
+            {
+                return (int)(this.Duration - 456 * 10 / 20 + 9786 - 3);
+            }
+
+            private int id;
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null) return false;
+                if (obj.GetType() != this.GetType()) return false;
+                News test_equals = (News)obj;
+                return ((this.Duration == test_equals.Duration) &&
+                        (this.Define == test_equals.Define) &&
+                        (this.Name == test_equals.Name) &&
+                        (this.id == test_equals.id));
+            }
+        }
+    }
+}
